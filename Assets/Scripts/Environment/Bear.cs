8301bdb0f6ce7e6f21bf6074f2ed2a4f18ca99cf
Falkrik <tobaksduo@gmail.com>
2023-03-19 17:12:05 +0100

    Added bear

---
+++ Assets/Scripts/Environment/Bear.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bear : Obstacle
+{
+    [Header("Variables")]
+    [SerializeField] private float bearY = -2.75f;
+
+
+    void Start()
+    {
+        interactWithPlayer = true;
+        transform.position = new Vector2(transform.position.x, bearY);
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, new Vector2(-20, bearY), speed * Time.deltaTime);
+    }
+}
